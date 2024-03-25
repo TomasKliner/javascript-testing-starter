1876266fc6bfb6322ce4b18c6873bc190dbffd32
TomasKliner <tomasklinerr@gmail.com>
2024-03-25 11:11:46 +0100

    modified code, passing tests

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -5,6 +5,9 @@ describe("max", () => {
     it('should return the larger of two numbers', () => {
         expect(max(1, 2)).toBe(2);
         expect(max(2, 1)).toBe(2);
+    })
+
+    it("should return first number if they are equal", () => {
         expect(max(1, 1)).toBe(1);
     })
 })
