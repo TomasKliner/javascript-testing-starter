111d578c44197a6b196a61f0cdab0160215479bc
TomasKliner <tomasklinerr@gmail.com>
2024-03-25 11:09:32 +0100

    first test

---
+++ tests/intro.test.js
@@ -0,0 +1,10 @@
+import { describe, test, it, expect } from 'vitest';
+import {max} from '../src/intro.js'
+
+describe("max", () => {
+    it('should return the larger of two numbers', () => {
+        expect(max(1, 2)).toBe(2);
+        expect(max(2, 1)).toBe(2);
+        expect(max(1, 1)).toBe(1);
+    })
+})
